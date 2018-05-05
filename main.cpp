64f2e955261418ce30ca53bc3e3273cec060c5a1
trolich <35908525+trolich@users.noreply.github.com>
2018-05-05 17:19:04 -0600

    Merge and Search
    Implemented merge sort and binary search, tested both funtions

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 // 5/5/18
 // main.cpp
 
-#include<vector>
+#include<algorithm>
 #include<iostream>
+#include<vector>
 
 using namespace std;
 using std::cout;
+using std::sort;
+using std::equal;
 
 template <class T>
 void QuickSort(vector<T> & vec);
@@ -18,17 +21,31 @@ void QuickSortHelper(vector<T> & vec, int first, int last);
 template <class T>
 int partition(vector<T> & vec, int first, int last);
 
+template <class T>
+void MergeSort(vector<T> & vec);
+
+template <class T>
+bool binarySearch(vector<T> & vec, T item);
+
+template <class T>
+int MyHash(T word);
+
 int main()
 {
 	vector<char> vec = { 'a','b','g','c','f','a' };
+	vector<char> vec2 = vec;
+	vector<char> vec3 = vec;
+
 	QuickSort(vec);
-	vector<char>::iterator it = vec.begin();
-	while (it != vec.end())
-	{
-		cout << *it;
-		cout << endl;
-		it++;
-	}
+	sort(vec2.begin(), vec2.end());
+	cout << (vec == vec2) << endl;
+
+	MergeSort(vec3);
+	cout << (vec == vec3 && vec2 == vec3) << endl;
+
+	cout << binarySearch(vec, 'w') << endl;
+	cout << binarySearch(vec, 'a') << endl;
+
 	return 0;
 }
 
@@ -81,4 +98,99 @@ int partition(vector<T> & vec, int first, int last)
 	vec[right] = temp;
 
 	return right;
+}
+
+template <class T>
+void MergeSort(vector<T> & vec)
+{
+	if (vec.size() > 1)
+	{
+		int mid = vec.size() / 2;
+		vector<T> left;
+		vector<T> right;
+		typename vector<T>::iterator it = vec.begin();
+		int w = 0;
+		while (it != vec.end())
+		{
+			if (w < mid)
+				left.push_back(*it);
+			else
+				right.push_back(*it);
+			it++; w++;
+		}
+
+		MergeSort(left);
+		MergeSort(right);
+
+		unsigned int i = 0;
+		unsigned int j = 0;
+		unsigned int k = 0;
+		while ((i < left.size()) && (j < right.size()))
+		{
+			if (left[i] < right[j])
+			{
+				vec[k] = left[i];
+				i++;
+			}
+			else
+			{
+				vec[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < left.size())
+		{
+			vec[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < right.size())
+		{
+			vec[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+}
+
+template <class T>
+bool binarySearch(vector<T> & vec, T item)
+{
+	if (vec.size() == 0)
+		return false;
+	else if (vec.size() == 1)
+		return (vec[0] == item);
+	else
+	{
+		int mid = vec.size() / 2;
+
+		if (vec[mid] == item)
+			return true;
+		else
+		{
+			vector<T> left;
+			vector<T> right;
+			typename vector<T>::iterator it = vec.begin();
+			int i = 0;
+			while (it != vec.end())
+			{
+				if (i < mid)
+					left.push_back(*it);
+				else
+					right.push_back(*it);
+				it++; i++;
+			}
+			if (item < vec[mid])
+			{
+				return binarySearch(left, item);
+			}
+			else
+			{
+				return binarySearch(right, item);
+			}
+		}
+	}
 }
