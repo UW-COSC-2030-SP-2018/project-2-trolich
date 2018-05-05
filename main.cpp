7a82b3fd60f4734118d5f132da1a90ff5f96fd44
trolich <35908525+trolich@users.noreply.github.com>
2018-05-05 15:27:28 -0600

    Quick Sort
    fixed quick sort so it actually works, also tested it in main

--- main.cpp
+++ main.cpp
@@ -7,96 +7,78 @@
 #include<iostream>
 
 using namespace std;
+using std::cout;
 
-template <typename T>
-vector<T> QuickSort(vector<T> vec);
+template <class T>
+void QuickSort(vector<T> & vec);
+
+template <class T>
+void QuickSortHelper(vector<T> & vec, int first, int last);
+
+template <class T>
+int partition(vector<T> & vec, int first, int last);
 
 int main()
 {
+	vector<char> vec = { 'a','b','g','c','f','a' };
+	QuickSort(vec);
+	vector<char>::iterator it = vec.begin();
+	while (it != vec.end())
+	{
+		cout << *it;
+		cout << endl;
+		it++;
+	}
 	return 0;
 }
 
-template <typename T>
-vector<T> QuickSort(vector<T> vec) {
-	vector<T> sorted = vec;
+template<class T>
+void QuickSort(vector<T> & vec)
+{
+	return QuickSortHelper(vec, 0, vec.size() - 1);
+}
 
-	if (sorted.size() > 1)
+template <class T>
+void QuickSortHelper(vector<T> & vec, int first, int last) 
+{
+	if (first < last)
 	{
-		T pivot = vec[0];
-		int leftMark = 1;
-		int rightMark = vec.size() - 1;
+		int split = partition(vec, first, last);
 
-		bool done = false;
-		while (!done)
-		{
-			while (leftMark <= rightMark && sorted[leftMark] <= pivot)
-				leftMark++;
-			while (sorted[rightMark] >= pivot && rightMark >= leftMark)
-				rightMark--;
-
-			if (rightMark < leftMark)
-				done = true;
-			else // Switch leftMark and rightMark values
-			{
-				T temp = sorted[leftMark];
-				sorted[leftMark] = sorted[rightMark];
-				sorted[rightMark] = temp;
-			}
-		}
-
-		// Switch rightMark with the pivot
-		T temp = pivot;
-		pivot = sorted[rightMark];
-		sorted[rightMark] = temp;
-
-
-		// Split vector into 3, left, pivot, right
-		vector<T> left;
-		vector<T> middle;
-		vector<T> right;
-		typename vector<T>::iterator it = sorted.begin();
-		while (it != sorted.end())
-		{
-			if (*it < pivot)
-			{
-				left.push_back(*it);
-			}
-			else if (*it > pivot)
-			{
-				right.push_back(*it);
-			}
-			else
-			{
-				middle.push_back(*it);
-			}
-			it++;
-		}
-
-		// Sort left and right
-		left = QuickSort(left);
-		right = QuickSort(right);
-		sorted.clear();
-
-		// Concatenate vectors and pivot
-		it = left.begin();
-		while (it != left.end())
-		{
-			sorted.push_back(*it);
-			it++;
-		}
-		it = middle.begin();
-		while (it != middle.end())
-		{
-			sorted.push_back(*it);
-			it++;
-		}
-		it = right.begin();
-		while (it != right.end())
-		{
-			sorted.push_back(*it);
-			it++;
-		}
-
+		QuickSortHelper(vec, first, split -1);
+		QuickSortHelper(vec, split + 1, last);
 	}
-	return sorted;
+}
+	
+template <class T>
+int partition(vector<T> & vec, int first, int last)
+{
+	T pivot = vec[first];
+
+	int left = first + 1;
+	int right = last;
+
+	bool done = false;
+	while (!done)
+	{
+		while (left <= right && vec[left] <= pivot)
+			left = left + 1;
+		while (vec[right] >= pivot && right >= left)
+			right = right - 1;
+
+		if (right < left)
+			done = true;
+		else
+		{
+			T temp = vec[left];
+			vec[left] = vec[right];
+			vec[right] = temp;
+		}
+	}
+
+	T temp = vec[first];
+	vec[first] = vec[right];
+	vec[right] = temp;
+
+	return right;
 }
